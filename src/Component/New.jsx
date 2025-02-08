c83bfe692a6bc9f05bc354391ae1ea2230ef77fc
tushal024 <tushalshekh@gmail.com>
2025-02-08 23:47:25 +0530

    Ad

---
+++ src/Component/New.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const New = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit id nihil minus expedita dicta cumque laborum doloremque libero eligendi sequi?
+      </p>
+    </div>
+  )
+}
+
+export default New
